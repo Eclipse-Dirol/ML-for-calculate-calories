263cdde7c01dd528c58d4cca20744d7826a9c532
Gleb Bikbaev <bikbaevgleb4@gmail.com>
2026-02-21 14:01:15 +0500

    added conn.close() in func

--- work_with_db.py
+++ work_with_db.py
@@ -11,9 +11,10 @@ class work_with_db():
     def db_entry_from_df(self, df: pd.DataFrame):
         df.to_sql('data', con=self.conn, if_exists='append', index=False)
         
-    def action_with_db(self, action, columns):
+    def action_with_db(self, action, columns, close=False):
         placeholder = ', '.join(columns)
         data = self.cursor.execute(f'{action} {placeholder} FROM data').fetchall()
         self.conn.commit()
-        self.conn.close()
+        if close:
+            self.conn.close()
         return data
