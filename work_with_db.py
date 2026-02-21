ae104b69c308f6283a195b172cb5b91a3f23fa2d
Gleb Bikbaev <bikbaevgleb4@gmail.com>
2026-02-21 13:26:26 +0500

    finally created class work_with_db

--- work_with_db.py
+++ work_with_db.py
@@ -1,19 +1,19 @@
 import sqlite3 as sql
 import pandas as pd
+import os
 
 class work_with_db():
     def __init__(self):
-        self.conn = sql.connect('data/database.db')
+        db_path = os.path.join(os.path.dirname(__file__), 'data', 'database.db')
+        self.conn = sql.connect(db_path)
         self.cursor = self.conn.cursor()
-    
-    def import_from_db(self):
-        pass
-    
-    def db_entry(self):
-        pass
-    
+        
     def db_entry_from_df(self, df: pd.DataFrame):
         df.to_sql('data', con=self.conn, if_exists='append', index=False)
-    
-    def update_db(self):
-        pass
+        
+    def action_with_db(self, action, columns):
+        placeholder = ', '.join(columns)
+        data = self.cursor.execute(f'{action} {placeholder} FROM data').fetchall()
+        self.conn.commit()
+        self.conn.close()
+        return data
