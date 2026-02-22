abf60cbb9332c3b567a9034bb425f0b1565f5293
Gleb Bikbaev <bikbaevgleb4@gmail.com>
2026-02-22 18:16:08 +0500

    realized train_test_data func

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,22 +4,32 @@ from work_with_db import work_with_db
 from sklearn.decomposition import PCA
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+import torch
 
 class preprocessing():
     def __init__(self):
         self.db = work_with_db()
         self.pca = PCA()
-        self.data = pd.DataFrame(self.db.action_with_db(action='SELECT', columns=['id', 'Sex', 'Age', 'Height', 'Weight', 'Duration', 'Heart_Rate', 'Body_Temp']), columns=['id', 'Sex', 'Age', 'Height', 'Weight', 'Duration', 'Heart_Rate', 'Body_Temp'])
-        self.target = pd.DataFrame(self.db.action_with_db(action='SELECT', columns=['id', 'Calories'], close=True), columns=['id', 'Calories'])
+        self.data = pd.DataFrame(
+        self.db.action_with_db(action='SELECT', columns=['id', 'Sex', 'Age', 'Height', 'Weight', 'Duration', 'Heart_Rate', 'Body_Temp', 'Calories']),
+        columns=['id', 'Sex', 'Age', 'Height', 'Weight', 'Duration', 'Heart_Rate', 'Body_Temp', 'Calories']
+    )
+
         
     def train_test_data(self):
-        train_target = self.target[self.target['Calories'].notna() == True]
-        test_target = self.target[self.target['Calories'].notna() == False]
-        self.train_data = self.data.merge(train_target, on='id').drop(columns='id')
-        self.test_data = self.data.merge(test_target, on='id').drop(columns='id')
+        train_mask = self.data['Calories'].notna()
+        self.train_data = self.data.loc[train_mask].drop(columns=['id', 'Calories'])
+        self.train_target = self.data.loc[train_mask, 'Calories']
+        self.test_data = self.data.loc[~train_mask].drop(columns=['id', 'Calories'])
         
     def col_trans(self):
-        cat_cols = self.train_data.select_dtypes(include=['object', 'category']).columns.tolist()
-        num_cols = self.train_data.select_dtypes(include=['int64', 'float64']).columns.tolist()
-        
+        pass
+    
+    def to_tensor(self):
+        pass
+    
+    def process(self):
+        self.train_test_data()
+        self.col_trans()
+        return self.to_tensor()
         
