9d88a9b5fff3e76bd2d295044768e3db6785dcff
Gleb Bikbaev <bikbaevgleb4@gmail.com>
2026-02-25 12:49:40 +0500

    saved the weights of the models

--- service.py
+++ service.py
@@ -3,20 +3,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 class ClientData(BaseModel):
-    Sex: list
+    Male: int
+    Female: int
     Age: int
     Height: float
     Weight: float
     Duration: float
-    Heart_Rate: float
-    Body_Temp: float
     
 app = FastAPI()
 
 @app.post('/score')
 def score(data: ClientData):
-    if data.Height > 150 and data.Age > 20:
-        score = random.randint(1, 400)
-    else:
-        score = 500
+    score = 500
     return {'Calories': score}
